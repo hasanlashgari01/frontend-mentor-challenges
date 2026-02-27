d4051584b69a9a92c3a1bcf78fc13513c00085e4
Hasan Lashgari <hasanlashgari01@gmail.com>
2026-02-28 01:51:52 +0330

    create component - button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/header/header.tsx";
 import Status from "@/components/status/status.tsx";
 import Dropdown from "@/components/dropdown/dropdown.tsx";
+import Button from "@/components/button/button.tsx";
 
 function App() {
   return (
@@ -20,6 +21,21 @@ function App() {
               values={["timed(60s)", "passage"]}
               defaultValue="timed(60s)"
             />
+          </div>
+        </div>
+        <div className="relative mt-6">
+          <p className="leading-relaxed text-neutral-500 sm:text-xl lg:text-2xl">
+            The archaeological expedition unearthed artifacts that complicated
+            prevailing theories about Bronze Age trade networks. Obsidian from
+            Anatolia, lapis lazuli from Afghanistan, and amber from the
+            Baltic—all discovered in a single Mycenaean tomb—suggested
+            commercial connections far more extensive than previously
+            hypothesized. "We've underestimated ancient peoples' navigational
+            capabilities and their appetite for luxury goods," the lead
+            researcher observed. "Globalization isn't as modern as we assume."
+          </p>
+          <div className="absolute inset-0 flex items-center justify-center backdrop-blur-xs">
+            <Button>Start Typing Test</Button>
           </div>
         </div>
       </div>
